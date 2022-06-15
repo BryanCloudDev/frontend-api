cb5edd6431ce03fb1cb5a092fa06b0bca6ac6584
Alejandroq12 <quezada.rodriguez6@gmail.com>
2022-06-15 16:14:19 -0600

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,33 @@
 <body>
     @include('modules.navbar')
     <div class="mapMaxSize container animate__animated animate__fadeInDown" id="app"></div>
-    @include('modules.footer')
+    <section>
+    <h2>States</h2>
+    <div class="col-sm-3 mt-4">
+    <div class="card">
+                        <div class="card-body card-body-color">
+                            <h5 class="card-header-color text-danger">States</h5>
+                            <dl>
+                                <dt>Modificar1</dt>
+                                <dd>Modificar2</dd>
+                            </dl>
+        </div>
+    </div>
+
+    <h2>Vaccines</h2>
+    <div class="card">
+                        <div class="card-body card-body-color">
+                            <h5 class="card-header-color text-danger">Vaccines</h5>
+                            <dl>
+                                <dt>Modificar1</dt>
+                                <dd>Modificar2</dd>
+                            </dl>
+        </div>
+    </div>
+</section>
+    <section>
+        @include('modules.footer')
+</section>
 </body>
 <script src="{{asset('../js/main.js')}}" defer></script>
 </html>
