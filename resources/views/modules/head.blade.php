5c3fde30c04947f938e088b3060067d3934e1f25
Bryan Portillo <bportillo701@gmail.com>
2022-06-16 13:08:18 -0600

    adding links to footer

--- resources/views/modules/head.blade.php
+++ resources/views/modules/head.blade.php
@@ -3,6 +3,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<script src="https://use.fontawesome.com/d034c4f984.js"></script>
 <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700;900&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
 <link href="{{ asset('../css/app.css') }}" rel="stylesheet">
